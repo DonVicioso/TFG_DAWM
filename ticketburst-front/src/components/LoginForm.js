466c6295bdfe2732cd8ec24706a537fc66b87530
Aarón Blázquez Martín <aaronblazquezmartin24@gmail.com>
2024-03-25 00:10:46 +0100

    forumalarioLogin-register

--- ticketburst-front/src/components/LoginForm.js
+++ ticketburst-front/src/components/LoginForm.js
@@ -6,16 +6,21 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 export default function LoginForm(){
 
+    const [loginDiv, setLoginDiv] = useState(true)
     const [form, setForm] = useState({
         username: "",
         password:""
     })
 
-    
+    function showRegister(){
+      setLoginDiv(prevState=>!prevState)
+    }
+
     return (
       <div className="loginForm">
         <Container>
           <Row>
+            {loginDiv &&
             <Col xs={12} lg={6} md={6} className='d-flex justify-content-start align-items-center'>
               <form className="w-100 d-flex  flex-column align-items-center gap-3">
                 
@@ -34,11 +39,49 @@ export default function LoginForm(){
                 
                 <div className='d-flex gap-3'>
                     <Button variant="outline-secondary" className="button-hover-white">Login</Button>
-                    <Button variant="outline-secondary" className='button-hover-white'>Registrarse</Button>
+                    <Button variant="outline-secondary" className='button-hover-white' onClick={showRegister}>Registrarse</Button>
                 </div>
                 
               </form>
             </Col>
+            }
+            {
+              !loginDiv &&
+              <Col xs={12} lg={6} md={6} className='d-flex justify-content-start align-items-center'>
+              <form className="w-100 d-flex  flex-column align-items-center gap-3">
+                
+                <Form.Control
+                  type="email"
+                  placeholder="example@mail.com"
+                  aria-label="email"
+                />
+
+                <Form.Control
+                  type="text"
+                  placeholder="Username"
+                  aria-label="username"
+                />
+                
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  aria-label="password"
+                />
+
+                <Form.Control
+                  type="password"
+                  placeholder="Repeat Password"
+                  aria-label="password2"
+                />
+                
+                <div className='d-flex gap-3 align-items-center'>
+                    <Button variant="outline-secondary" className='button-hover-white'>Registrarse</Button>
+                    or <label onClick={showRegister}>Login</label>
+                </div>
+                
+              </form>
+            </Col>
+            }
             <Col xs={6}>
                 
             </Col>
